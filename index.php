b5b0decf84c980f011b5017a7dc0205e98d2f66d
CorruptedCube <64045580+CorruptedCube@users.noreply.github.com>
2024-05-07 22:49:07 +0200

    Update index.php
    gekke nieuwe site hierow

--- index.php
+++ index.php
@@ -1,3 +1,389 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-echo "clennutretion.nl!";
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <link rel="shortcut icon" href="images/slider-img.png" type="image/x-icon">
+
+  <title>
+    Clen Nutretion
+  </title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <nav class="navbar navbar-expand-lg custom_nav-container ">
+        <a class="navbar-brand" href="index.html">
+          <span>
+            Clen Nutretion
+          </span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class=""></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav  ">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="shop.html">
+                Winkl
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">kontakt Onz</a>
+            </li>
+          </ul>
+          <div class="user_option">
+            <a href="">
+              <i class="fa fa-user" aria-hidden="true"></i>
+              <span>
+                Lokin
+              </span>
+            </a>
+            <a href="">
+              <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- end header section -->
+    <!-- slider section -->
+
+    <section class="slider_section">
+      <div class="slider_container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-7">
+                    <div class="detail-box">
+                      <h1>
+                        Welkum <br>
+                        Clen Nutretion
+                      </h1>
+                      <p>
+                        Best Suplment hIer Kop! Samn Foor Gud.
+                      </p>
+                      <a href="">
+                        kontakt onz
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-md-5 ">
+                    <div class="img-box">
+                      <img src="images/slider-img.png" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+  
+          <div class="carousel_btn-box">
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <i class="fa fa-arrow-left" aria-hidden="true"></i>
+              <span class="sr-only">Previous</span>
+            </a>
+            <img src="images/line.png" alt="" />
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <i class="fa fa-arrow-right" aria-hidden="true"></i>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- end slider section -->
+  </div>
+  <!-- end hero area -->
+
+  <!-- shop section -->
+
+  <section class="shop_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Niewzte Protukden!
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/p1.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Kreadiene
+                </h6>
+                <h6>
+                  Preis
+                  <span>
+                    $39.39
+                  </span>
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  Niew
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/p2.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Bomp Pabaraad
+                </h6>
+                <h6>
+                  Preis
+                  <span>
+                    $29.29
+                  </span>
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  Niew
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/p3.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Clen Nutretion Ghoodini
+                </h6>
+                <h6>
+                  Preis
+                  <span>
+                    $169.42
+                  </span>
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  niew
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/p4.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Clen Nutretion D-Sirt
+                </h6>
+                <h6>
+                  Preis
+                  <span>
+                    $69.69
+                  </span>
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  niew
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+
+      </div>
+      <div class="btn-box">
+        <a href="">
+          Bekeik allez
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- end shop section -->
+
+  <!-- saving section -->
+
+  <section class="saving_section ">
+    <div class="box">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="img-box">
+              <img src="images/slider-img.png" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="detail-box">
+              <div class="heading_container">
+                <h2>
+                  Beste Koin Sam Foor Gud.<br>
+                  
+                </h2>
+              </div>
+              <p>
+               Khoop oP Solana! Noe Gud outo! Lamboghirni eN Ferfari Nar De mHan!
+              </p>
+              <div class="btn-box">
+                <a href="#" class="btn1">
+                  Khoop
+                </a>
+                <a href="#" class="btn2">
+                  Leez Mer
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end saving section -->
+
+  <!-- why section -->
+
+ 
+  <!-- info section -->
+
+  <section class="info_section  layout_padding2-top">
+    <div class="social_container">
+      <div class="social_box">
+        <a href="">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+        </a>
+      </div>
+    </div>
+    <div class="info_container ">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              Ofer Onz
+            </h6>
+            <p>
+              Wei peste komunitie! Sam Foor Gud! Brek Ti gele Dent Av!
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="info_form ">
+              <h5>
+                NiewzPriev
+              </h5>
+              <form action="#">
+                <input type="email" placeholder="geev Emeel!">
+                <button>
+                  Apponeer
+                </button>
+              </form>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              gulp Notig?
+            </h6>
+            <p>
+              Kontakt Onz! Wei Gelpen!
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              Kontakt Onz
+            </h6>
+            <div class="info_link-box">
+              <a href="">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span> Foor GudStrat </span>
+              </a>
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>+6942069400kbtc</span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span> julbekers@clennutretion.com</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- footer section -->
+    <footer class=" footer_section">
+      <div class="container">
+        <p>
+          &copy; <span id="displayYear"></span> All Rights Reserved By
+          <a href="https://html.design/">Je dikke moeder</a>
+        </p>
+      </div>
+    </footer>
+    <!-- footer section -->
+
+  </section>
+
+  <!-- end info section -->
+
+
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <script src="js/custom.js"></script>
+
+</body>
+
+</html>
